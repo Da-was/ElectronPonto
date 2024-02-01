92a2aeec79225bd3d42b72b82f7b2548c66b055b
joão laurindo <joaoviniciuslaurindo@live.com>
2024-02-01 12:09:02 -0300

    devido a problemas com o python no meu computador, vou deixar a detecção de faces de lado e me focar no moongose agora.

--- render/javascript/faceDetection/cameraStart.js
+++ render/javascript/faceDetection/cameraStart.js
@@ -1,18 +1,17 @@
-Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
-  faceapi.nets.tinyFaceDetector.loadFromUri("./models"), //stoped here
-]);
-//Esperar a webcam nova para testar
-navigator.mediaDevices
-  .getUserMedia({ video: true })
-  .then((stream) => {
-    document.getElementById("loadingGif").remove();
+function startVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((stream) => {
+      document.getElementById("loadingGif").remove();
 
-    const videoOutuput = document.createElement("video");
+      const videoOutuput = document.createElement("video");
 
-    videoOutuput.setAttribute("autoplay", "true");
-    videoOutuput.setAttribute("id", "cameraOutput");
-    document.getElementById("cameraBackground").appendChild(videoOutuput);
-    videoOutuput.srcObject = stream;
-  })
-  .catch((error) => alert(error));
+      videoOutuput.setAttribute("autoplay", "true");
+      videoOutuput.setAttribute("id", "cameraOutput");
+      document.getElementById("cameraBackground").appendChild(videoOutuput);
+      videoOutuput.srcObject = stream;
+    })
+    .catch((error) => alert(error));
+}
+
+startVideo();
