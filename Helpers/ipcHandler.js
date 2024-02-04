0e3bed4d038cd1afd47337e832613b26b5de6bfa
joão laurindo <joaoviniciuslaurindo@live.com>
2024-02-04 20:37:45 -0300

    Melhorando organização do código

--- Helpers/ipcHandler.js
+++ Helpers/ipcHandler.js
@@ -4,31 +4,33 @@ const Ponto = require("../models/Ponto.js");
 const { version } = require("../package.json");
 
 module.exports = function handleIpc() {
-  ipcMain.handle("ping", () => "pong");
   ipcMain.handle("version", () => version);
-  ipcMain.handle("getMembros", async () => {
-    let memberList = [];
-    await Membro.find({}).then(function (membros) {
-      membros.forEach((membro) => {
-        membro = membro.toObject();
-        membro._id = membro._id.toString();
-        memberList.push(membro);
-      });
-    });
-    return memberList;
-  });
-
-  ipcMain.handle("getPontos", async () => {
-    let pontoList = [];
-
-    const pontos = await Ponto.find({}).populate("membro").exec();
-    pontos.forEach((ponto) => {
-      ponto = ponto.toObject();
-      ponto._id = ponto._id.toString();
-      ponto.membro._id = ponto.membro._id.toString();
-      pontoList.push(ponto);
-    });
-
-    return pontoList;
-  });
+  ipcMain.handle("getMembros", getMembros);
+  ipcMain.handle("getPontos", getPontos);
 };
+
+async function getMembros() {
+  let memberList = [];
+  await Membro.find({}).then(function (membros) {
+    membros.forEach((membro) => {
+      membro = membro.toObject();
+      membro._id = membro._id.toString();
+      memberList.push(membro);
+    });
+  });
+  return memberList;
+}
+
+async function getPontos() {
+  let pontoList = [];
+
+  const pontos = await Ponto.find({}).populate("membro").exec();
+  pontos.forEach((ponto) => {
+    ponto = ponto.toObject();
+    ponto._id = ponto._id.toString();
+    ponto.membro._id = ponto.membro._id.toString();
+    pontoList.push(ponto);
+  });
+
+  return pontoList;
+}
