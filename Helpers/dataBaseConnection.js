9ce0227698ea299eff69306315e2fbff95a52feb
joão laurindo <joaoviniciuslaurindo@live.com>
2024-02-05 11:07:29 -0300

    Alterando versão e removendo comentário descenessário.

--- Helpers/dataBaseConnection.js
+++ Helpers/dataBaseConnection.js
@@ -7,15 +7,6 @@ module.exports = async () => {
     .connect(process.env.mongoToken)
     .then(async () => {
       console.log("Connected to mongo");
-
-      //aprendendo a mexer no mongo
-      /* const test = await Membro.findById("65bbfc16d56f7ed8543784b8").exec();
-
-      const pessoa = await Ponto.create({
-        membro: test._id,
-      });
-
-      console.log(pessoa); */
     })
     .catch((error) => console.error(error));
 };
