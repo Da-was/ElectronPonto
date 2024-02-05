2b4ce30e1e68a76e3898b638aeba99303ca0d2c4
joão laurindo <joaoviniciuslaurindo@live.com>
2024-02-05 12:38:21 -0300

    Menssagens de suporte e melhoria nas integrações.

--- Helpers/dataBaseConnection.js
+++ Helpers/dataBaseConnection.js
@@ -1,12 +1,8 @@
-const Membro = require("../models/Membro.js");
-const Ponto = require("../models/Ponto.js");
 const mongoose = require("mongoose");
 
 module.exports = async () => {
   await mongoose
     .connect(process.env.mongoToken)
-    .then(async () => {
-      console.log("Connected to mongo");
-    })
+    .then(() => console.log("Connected to mongo"))
     .catch((error) => console.error(error));
 };
--- index.js
+++ index.js
@@ -3,9 +3,10 @@ require("dotenv").config();
 const dataBaseConnection = require("./Helpers/dataBaseConnection.js");
 
 const path = require("node:path");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, dialog } = require("electron");
 const ipcHandle = require("./Helpers/ipcHandler.js");
 const checkIntegration = require("./Helpers/checkIntegration.js");
+const discordSupport = require("./Integrations/discord-support.js");
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -18,11 +19,34 @@ function createWindow() {
   win.loadFile("./render/html/index.html");
 }
 
+async function handleEror(error) {
+  const messageBoxOptions = {
+    type: "error",
+    title: "Falha no processo principal",
+    message: "Uma mensagem foi enviada ao suporte",
+  };
+
+  if (process.env.debug === "true") {
+    messageBoxOptions.message = error.toString();
+  } else {
+    if (checkIntegration.integrations.discord) {
+      await discordSupport.sendMessage(error.toString());
+    }
+  }
+
+  dialog.showMessageBoxSync(messageBoxOptions);
+  app.exit(1);
+}
+
 function initializeAll() {
-  checkIntegration();
-  dataBaseConnection();
-  createWindow();
-  ipcHandle();
+  try {
+    checkIntegration.checkAvaible();
+    dataBaseConnection();
+    createWindow();
+    ipcHandle();
+  } catch (error) {
+    handleEror(error);
+  }
 }
 
 app.whenReady().then(initializeAll);
